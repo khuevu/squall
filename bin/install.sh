0ae304c639f923ea4db048b5abefb2cec235baa2
Khue Vu <khuevu@gmail.com>
2015-04-13 16:53:07 +0200

    Add SquallToast dependencies (all new jars are needed for DBToaster)

--- bin/install.sh
+++ bin/install.sh
@@ -9,7 +9,7 @@ cp ../contrib/leiningen-1.7.1-standalone.jar $HOME/.lein/self-installs/
 ./lein localrepo install ../contrib/je-5.0.84.jar bdb-je 5.0.84
 ./lein localrepo install ../contrib/ujmp-complete-0.2.5.jar ujmp 0.2.5
 ./lein localrepo install ../contrib/opencsv-2.3.jar opencsv 2.3
-# dbtoaster
+# dbtoaster-dependencies
 ./lein localrepo install ../contrib/dbtoaster/akka-actor_2.10-2.2.3.jar akka 2.2.3 
 ./lein localrepo install ../contrib/dbtoaster/cal10n-api-0.7.4.jar cal10 0.7.4 
 ./lein localrepo install ../contrib/dbtoaster/config-1.0.2.jar config 1.0.2 
@@ -19,10 +19,8 @@ cp ../contrib/leiningen-1.7.1-standalone.jar $HOME/.lein/self-installs/
 ./lein localrepo install ../contrib/dbtoaster/scala-reflect-2.10.2-RC1.jar scala-reflect 2.10.2-RC1
 ./lein localrepo install ../contrib/dbtoaster/scalariform_2.10-0.1.4.jar scalariform 0.1.4
 ./lein localrepo install ../contrib/dbtoaster/uncommons-maths-1.2.2a.jar uncommons-maths 1.2.2a
-
-#tmp
-./lein localrepo install /Users/khuevu/Projects/DDBToaster/tmp/query.jar dbtoasterquery 0.0.1
-./lein localrepo install /Users/khuevu/Projects/DDBToaster/tmp/dbtoaster.jar dbtoaster 0.0.1
+# dbtoaster 
+./lein localrepo install ../contrib/dbtoaster/dbtoaster.jar dbtoaster 0.0.1
 
 #copy appropriate version of storm.yaml on local machine
 #mkdir -p ~/.storm
